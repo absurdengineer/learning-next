75b5f52a300ba052fe9e062d6683e125a2a2cfb1
Mohammad Dilshad Alam <mddalam1@gmail.com>
2025-01-11 02:07:12 +0100

    feat: custom not found page for user details

---
+++ app/users/[id]/not-found.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const UserNotFoundPage = () => {
+  return (
+    <div className="flex justify-center">User with the given ID not found.</div>
+  );
+};
+
+export default UserNotFoundPage;
--- app/users/[id]/page.tsx
+++ app/users/[id]/page.tsx
@@ -1,9 +1,13 @@
+import { notFound } from "next/navigation";
+
 interface Props {
   params: Promise<{ id: number }>;
 }
 
 const UserDetailPage = async ({ params }: Props) => {
   const { id } = await params;
+
+  if (id > 10) notFound();
   return <div>UserDetailPage {id}</div>;
 };
 
