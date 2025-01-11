b365ac91778834b3633ccc02dd52f3be142500a2
Mohammad Dilshad Alam <mddalam1@gmail.com>
2025-01-11 13:28:10 +0100

    feat: implement POST endpoint for user creation with validation

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,10 +1,39 @@
 import { NextRequest, NextResponse } from "next/server";
 import users from "./users.json";
 
+let id = 1;
+
+const getUserId = () => {
+  if (id > 100) id = 1;
+  return (id += 1);
+};
+
+interface User {
+  name: string;
+  email: string;
+}
+
 /// request is not needed here, but it's a good practice and also
 /// nextjs cache the response if the request object is not included
 export const GET = (request: NextRequest) => {
   return NextResponse.json({
     data: users,
+  });
+};
+
+export const POST = async (request: NextRequest) => {
+  const user: User = await request.json();
+  if (!user || !user.name || !user.email)
+    return NextResponse.json(
+      {
+        error: "Name and email are required",
+      },
+      {
+        status: 400,
+      }
+    );
+  return NextResponse.json({
+    id: getUserId(),
+    ...user,
   });
 };
