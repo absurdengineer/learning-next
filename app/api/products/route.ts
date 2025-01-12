089cfe98ddc30cbce898722c9a8910d03e15080c
Mohammad Dilshad Alam <mddalam1@gmail.com>
2025-01-12 21:18:48 +0100

    feat: integrate Prisma for product retrieval and creation, removing in-memory product storage

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -1,23 +1,25 @@
 import { NextRequest, NextResponse } from "next/server";
-import { getNextProductId, products } from "./products";
+import { prisma } from "@/prisma/client";
 import productSchema from "./schema";
 
-export const GET = (request: NextRequest) => {
+export const GET = async (request: NextRequest) => {
+  const products = await prisma.product.findMany();
   return NextResponse.json({
     data: products,
   });
 };
 
 export const POST = async (request: NextRequest) => {
-  let product = await request.json();
-  const validation = productSchema.safeParse(product);
+  let body = await request.json();
+  const validation = productSchema.safeParse(body);
   if (!validation.success)
     return NextResponse.json(
       { error: validation.error.errors },
       { status: 400 }
     );
-  product = { id: getNextProductId(), ...product };
-  products.push(product);
+  const product = await prisma.product.create({
+    data: body,
+  });
   return NextResponse.json(
     {
       data: product,
